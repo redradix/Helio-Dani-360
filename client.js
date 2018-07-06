54630bafbe1cbfbda90ecf83d917e8d0aa1314c9
alejandrodob <alejandrodob@gmail.com>
2018-07-06 16:31:34 +0200

    Extract screen dimensions to config file

--- client.js
+++ client.js
@@ -1,30 +1,31 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import { ReactInstance, Surface } from "react-360-web";
+import { ReactInstance, Surface } from 'react-360-web'
+import { screenDimensions } from './config'
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
     // Add custom options here
     fullScreen: true,
     ...options
-  });
+  })
 
-  const s = r360.getDefaultSurface();
+  const s = r360.getDefaultSurface()
   //s.setShape(Surface.SurfaceShape.Flat);
   //s.resize(4096, 600);
-  s.resize(4680, 600);
+  s.resize(screenDimensions.width, screenDimensions.height)
 
   // Render your app content to the default cylinder surface
   r360.renderToSurface(
-    r360.createRoot("example_360", {
+    r360.createRoot('example_360', {
       /* initial props */
     }),
     s
-  );
+  )
 
   // Load the initial environment
-  r360.compositor.setBackground(r360.getAssetURL("Homepage+LEI.jpg"));
+  r360.compositor.setBackground(r360.getAssetURL('Homepage+LEI.jpg'))
 }
 
-window.React360 = { init };
+window.React360 = { init }
--- index.js
+++ index.js
@@ -1,66 +1,70 @@
-import React from "react";
-import { AppRegistry, StyleSheet, Text, View } from "react-360";
+import React from 'react'
+import { AppRegistry, StyleSheet, Text, View } from 'react-360'
+import { screenDimensions } from './config'
+
+const screenWidth = screenDimensions.width
+const screenHeight = screenDimensions.height
 
 export default class example_360 extends React.Component {
   state = {
     translation: 0
-  };
+  }
   componentDidMount() {
     setInterval(() => {
       this.setState(({ translation }) => {
-        let multiplier = translation <= 0 ? -1 : 1;
-        console.log(translation, -4680 / 2, 4680 / 2);
-        if (translation < -4680 / 2) {
+        let multiplier = translation <= 0 ? -1 : 1
+        console.log(translation, -screenWidth / 2, screenWidth / 2)
+        if (translation < -screenWidth / 2) {
           return {
-            translation: 4680 / 2
-          };
-        } else if (translation > 4680 / 2) {
+            translation: screenWidth / 2
+          }
+        } else if (translation > screenWidth / 2) {
           return {
-            translation: -4680 / 2
-          };
+            translation: -screenWidth / 2
+          }
         }
         return {
           translation: translation - 10
-        };
-      });
-    }, 50);
+        }
+      })
+    }, 50)
   }
   render() {
-    const { translation } = this.state;
-    console.log(translation);
+    const { translation } = this.state
+    console.log(translation)
     return (
       <View style={styles.panel}>
         <View
           style={[
-            styles.greetingBox,
+            styles.snakeContainer,
             { transform: [{ translate: [translation, 0, 0] }] }
           ]}
         >
-          <Text style={styles.greeting}>I'm a snakeeeeeeeeeeeeeeee</Text>
+          <Text style={styles.snake}>I'm a snakeeeeeeeeeeeeeeee</Text>
         </View>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   panel: {
     // Fill the entire surface
-    width: 4680,
-    height: 600,
-    backgroundColor: "rgba(255, 255, 255, 0.6)",
-    justifyContent: "center",
-    alignItems: "center"
+    width: screenWidth,
+    height: screenHeight,
+    backgroundColor: 'rgba(255, 255, 255, 0.6)',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  greetingBox: {
+  snakeContainer: {
     padding: 20,
-    backgroundColor: "#000000",
-    borderColor: "#639dda",
+    backgroundColor: '#000000',
+    borderColor: '#639dda',
     borderWidth: 2
   },
-  greeting: {
+  snake: {
     fontSize: 30
   }
-});
+})
 
-AppRegistry.registerComponent("example_360", () => example_360);
+AppRegistry.registerComponent('example_360', () => example_360)
