8fd9f256415a18775a628077c8fcd02a2dce40a3
alejandrodob <alejandrodob@gmail.com>
2018-07-20 16:49:55 +0200

    Moving the snake with the keyboard

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { AppRegistry, StyleSheet, Text, View } from "react-360";
 import { screenDimensions } from "./config";
 import makeGame from "./snake";
 
+const KEY_CODES = { 37: "LEFT", 38: "UP", 39: "RIGHT", 40: "DOWN" };
+
 const screenWidth = screenDimensions.width;
 const screenHeight = screenDimensions.height;
 const gameWidth = 60;
@@ -29,11 +31,14 @@ export default class App extends React.Component {
       this.setState(this.game.next(this.direction));
     }, 500);
   }
+  handleKeyPress = e => {
+    if (!KEY_CODES[e.nativeEvent.inputEvent.button]) return;
+    this.direction = KEY_CODES[e.nativeEvent.inputEvent.button];
+  };
   render() {
     const { snake, food } = this.state;
-
     return (
-      <View style={styles.panel}>
+      <View style={styles.panel} onInput={this.handleKeyPress}>
         {heightArray.map((_, y) => (
           <View style={{ flexDirection: "row" }}>
             {widthArray.map((_, x) => {
@@ -43,9 +48,7 @@ export default class App extends React.Component {
                 <View
                   key={`${x}-${y}`}
                   style={{
-                    borderColor: "red",
                     backgroundColor: color,
-                    borderWidth: 1,
                     width: cellWidth,
                     height: cellHeight
                   }}
--- snake.js
+++ snake.js
@@ -2,22 +2,31 @@ const RIGHT = "RIGHT";
 const LEFT = "LEFT";
 const UP = "UP";
 const DOWN = "DOWN";
+const xMultiplayer = { LEFT: -1, RIGHT: 1 };
+const yMultiplayer = { UP: -1, DOWN: 1 };
+const movingHorizontally = direction =>
+  direction === LEFT || direction === RIGHT;
+const movingVertically = direction => direction === UP || direction === DOWN;
 
 function mod(n, m) {
   return ((n % m) + m) % m;
 }
 
 const makeGame = (width, height) => {
-  let snake = new Array(4)
+  let snake = new Array(8)
     .fill()
     .map((_, i) => [Math.round(width / 2) - i, Math.round(height / 2)]);
 
   return {
     next: function next(direction) {
-      snake = snake.map(([x, y]) => [mod(x - 1, width), mod(y, height)]);
+      const xMult = movingHorizontally(direction) ? xMultiplayer[direction] : 0;
+      const yMult = movingVertically(direction) ? yMultiplayer[direction] : 0;
+      snake = snake.map(([x, y], i) => {
+        if (i === 0) return [mod(x + xMult, width), mod(y + yMult, height)];
+        return snake[i - 1];
+      });
       return {
-        snake,
-        food: [34, 27]
+        snake
       };
     }
   };
